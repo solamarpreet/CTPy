235396d6ce7fec41301a32ca84aef0a131c72cf2
Amarpreet Singh <solamarpreet@gmail.com>
2022-02-20 12:33:31 +0530

    Added Q12

--- server/questions.py
+++ server/questions.py
@@ -188,3 +188,21 @@ def q11():
             return jsonify({'check':'Incorrect Answer. Please try again'})
     else:
         return jsonify({'qnum': dict['qnum'], 'qtext': dict['qtext'], 'qdata': dict['qdata']})
+
+@question_bp.route('/12/', methods=['GET', 'POST'])
+def q12():
+    dict = {
+            'qnum'  :   '12',
+            'qtext' :   'Count the number of occurences of the word "the" in the string returned by game.data(12)',
+            'qdata' :   'Out of the night that covers me, Black as the pit from pole to pole. I thank whatever gods may be, For my unconquerable soul. In the fell clutch of circumstance, I have not winced nor cried aloud. Under the bludgeonings of chance, My head is bloody, but unbowed. Beyond this place of wrath and tears, Looms but the Horror of the shade. And yet the menace of the years, Finds and shall find me unafraid. It matters not how strait the gate, How charged with punishments the scroll. I am the master of my fate, I am the captain of my soul.',
+            'qans'  :   '12'
+            }
+    if request.method == 'POST':
+        ans = request.get_json()['ans']
+        if ans == dict['qans']:
+            return jsonify({'check':'Correct Answer'})
+        else:
+            return jsonify({'check':'Incorrect Answer. Please try again'})
+    else:
+        return jsonify({'qnum': dict['qnum'], 'qtext': dict['qtext'], 'qdata': dict['qdata']})
+
